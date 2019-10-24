1f4d0fa932df02c7c30bfdc9912ad797f88c8ac3
Vadym Liubko <neznam.ua@gmail.com>
2019-10-24 22:18:17 +0300

    port changed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 const request = require('request-promise');
 var fs = require('fs');
 
